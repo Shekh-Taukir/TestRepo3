df9f522559b46231f24701f0d0224b2dfc2b61d4
6113310 <6113310@TR-B7XHW33>
2022-05-17 13:44:23 +0530

    Add project files.

---
+++ DhartiMaaMvcCoreProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DhartiMaaMvcCoreProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ DhartiMaaProject/Models/DhartiMaaDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DhartiMaaProject.Models
+{
+    public class DhartiMaaDbContext : DbContext
+    {
+
+        public DhartiMaaDbContext(DbContextOptions<DhartiMaaDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+    }
+}
