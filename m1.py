d99c53e31891d69666271c4b25c0f2abe48d7520
Shekh Taukir <106509218+Shekh-Taukir@users.noreply.github.com>
2022-06-03 15:36:54 +0530

    Delete m1.py

--- m1.py
+++
@@ -1,6 +0,0 @@
-print("COde by Taukir Shekh")
-print("Name : Taukir Shekh ", 10 * 20)
-print(" value of a*b :", 10 * 30)
-
-print("Update file2, this is from dev1 branch, after adding super linter")
-print("this is error line, updated after secret key of github")
